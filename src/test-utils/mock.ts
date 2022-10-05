e040b2bcccd4fa62f806e618c6b1c15a51b5fee0
nemga123 <hspark0419@naver.com>
2022-10-05 21:50:29 +0900

    at practice session

---
+++ src/test-utils/mock.ts
@@ -0,0 +1,9 @@
+import { configureStore, PreloadedState } from "@reduxjs/toolkit";
+import { RootState } from "../store";
+import todoReducer from "../store/slices/todo";
+export const getMockStore = (preloadedState?: PreloadedState<RootState>) => {
+    return configureStore({
+        reducer: { todo: todoReducer },
+        preloadedState,
+        });
+    };
