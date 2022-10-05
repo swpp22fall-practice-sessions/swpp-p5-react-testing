b1d5378fab6606f587a86dd94885bfac5debe91a
Yujin Im <jina0605@gmail.com>
2022-10-05 19:41:25 +0900

    Add tests to Todo.test.tsx

--- src/components/Todo/Todo.test.tsx
+++ src/components/Todo/Todo.test.tsx
@@ -8,4 +8,16 @@ describe("<Todo />", () => {
     const doneButton = screen.getByText("Done"); // Implicit assertion
     expect(doneButton).toBeInTheDocument(); // Explicit assertion
   });
+  it("should render done mark when done is true", () => {
+    render(<Todo title={"TODO_TITLE"} done={true} />);
+    const title = screen.getByText("TODO_TITLE");
+    expect(title.classList.contains("done")).toBe(true);
+    screen.getByText("Undone");
+  });
+  it("should render undone mark when done is false", () => {
+    render(<Todo title={"TODO_TITLE"} done={false} />);
+    const title = screen.getByText("TODO_TITLE");
+    expect(title.classList.contains("done")).toBe(false);
+    screen.getByText("Done");
+  });
 });
