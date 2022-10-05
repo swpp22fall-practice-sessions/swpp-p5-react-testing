848905c4722798428818c227dd0657169ecbff19
Sanghyun Yi <shyi0211@gmail.com>
2022-10-05 21:00:45 +0900

    temp commit

--- src/components/TodoDetail/TodoDetial.test.tsx
+++ src/components/TodoDetail/TodoDetial.test.tsx
@@ -1,22 +1,11 @@
 import { fireEvent, render, screen } from "@testing-library/react";
 import { Provider } from "react-redux";
 import { MemoryRouter, Route, Routes } from "react-router";
+import { store } from "../../store";
 import { TodoState } from "../../store/slices/todo";
 import { getMockStore } from "../../test-utils/mocks";
 import TodoDetail from "./TodoDetail";
 
-jest.mock("../../components/TodoDetail/TodoDetail", () => (todoState: TodoState) => (
-    <div data-testid="spyTodoDetail">
-        <div className="row">
-            <div className="left">Name:</div>
-            <div className="right">{todoState.selectedTodo?.title}</div>
-        </div>
-        <div className="row">
-            <div className="left">Content:</div>
-            <div className="right">{todoState.selectedTodo?.content}</div>
-        </div>
-    </div>
-)); 
 const stubInitialState: TodoState = {
     todos: [
         { id: 1, title: "TODO_TEST_TITLE_1", content: "TODO_TEST_CONTENT_1", done: false },
@@ -25,65 +14,32 @@ const stubInitialState: TodoState = {
 };
 const mockStore = getMockStore({ todo: stubInitialState });
 
-const mockNavigate = jest.fn();
-jest.mock("react-router", () => ({
-    ...jest.requireActual("react-router"),
-    useNavigate: () => mockNavigate,
-}));
-const mockDispatch = jest.fn();
-jest.mock("react-redux", () => ({
-    ...jest.requireActual("react-redux"),
-    useDispatch: () => mockDispatch,
-}));
-
 describe("<TodoList />", () => {
     let todoList: JSX.Element;
     beforeEach(() => {
         jest.clearAllMocks();
         todoList = (
             <Provider store={mockStore}>
-                <MemoryRouter>
-                    <Routes>
-                        <Route path="/" element={<TodoList title="TODOLIST_TEST_TITLE" />} />
-                    </Routes>
-                </MemoryRouter>
+                <div data-testid="spyTodoDetail">
+                    <div className="row">
+                        <div className="left">Name:</div>
+                        <div className="right">{store.getState().todo.selectedTodo?.title}</div>
+                    </div>
+                    <div className="row">
+                        <div className="left">Content:</div>
+                        <div className="right">{store.getState().todo.selectedTodo?.content}</div>
+                    </div>
+                </div>
             </Provider>
         );
     });
-    it("should render TodoList", () => {
+    it("should render TodoDetail", () => {
         const { container } = render(todoList);
         expect(container).toBeTruthy();
     });
-    it("should render todos", () => {
+    it("should return error", () => {
         render(todoList);
         const todos = screen.getAllByTestId("spyTodo");
         expect(todos).toHaveLength(3);
-    });
-    it("should handle clickDetail", () => {
-        render(todoList);
-        const todos = screen.getAllByTestId("spyTodo");
-        const todo = todos[0];
-        // eslint-disable-next-line testing-library/no-node-access
-        const title = todo.querySelector(".title");
-        fireEvent.click(title!);
-        expect(mockNavigate).toHaveBeenCalledTimes(1);
-    });
-    it("should handle clickDone", () => {
-        render(todoList);
-        const todos = screen.getAllByTestId("spyTodo");
-        const todo = todos[0];
-        // eslint-disable-next-line testing-library/no-node-access
-        const doneButton = todo.querySelector(".doneButton");
-        fireEvent.click(doneButton!);
-        expect(mockDispatch).toHaveBeenCalled();
-        });
-    it("should handle clickDelete", () => {
-        render(todoList);
-        const todos = screen.getAllByTestId("spyTodo");
-        const todo = todos[0];
-        // eslint-disable-next-line testing-library/no-node-access
-        const deleteButton = todo.querySelector(".deleteButton");
-        fireEvent.click(deleteButton!);
-        expect(mockDispatch).toHaveBeenCalled();
     });
 });
