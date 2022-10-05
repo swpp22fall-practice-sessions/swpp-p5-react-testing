44c58baf3aa327878dbe0e3e2579336381e611cb
Quantee-OH <ohj3258@naver.com>
2022-10-05 16:20:56 +0900

    first

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,15 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import { Provider } from "react-redux";
 
 import App from "./App";
+import { store } from "./store";
 
-test("renders learn react link", () => {
-  render(<App />);
+test("renders App.tsx", () => {
+  render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+  screen.debug();
+  //expect(true).toBe(false);
 });
