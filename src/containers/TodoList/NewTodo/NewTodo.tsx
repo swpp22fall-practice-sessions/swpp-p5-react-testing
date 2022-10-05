fc611a120d6462f834e2f12295067c8fa2556917
gkgkgkgkgk73 <gkgkgkgkgk73@gmail.com>
2022-10-06 01:11:48 +0900

    1

--- src/containers/TodoList/NewTodo/NewTodo.tsx
+++ src/containers/TodoList/NewTodo/NewTodo.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { Navigate } from "react-router-dom";
-// import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { AppDispatch } from "../../../store";
 import { postTodo } from "../../../store/slices/todo";
 import "./NewTodo.css";
