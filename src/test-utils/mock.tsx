8afb3e7f0fd73a280d2d404d3cac639be9a67de7
2019-19835 <kmy17518@snu.ac.kr>
2022-10-06 11:47:06 +0900

    tododetail test

---
+++ src/test-utils/mock.tsx
@@ -0,0 +1,33 @@
+import { configureStore, PreloadedState } from "@reduxjs/toolkit";
+import { render, RenderOptions } from "@testing-library/react";
+import { PropsWithChildren } from "react";
+import { Provider } from "react-redux";
+import { AppStore, RootState } from "../store";
+import todoReducer from "../store/slices/todo";
+
+interface ExtendedRenderOptions extends Omit<RenderOptions, "queries"> {
+  preloadedState?: PreloadedState<RootState>;
+  store?: AppStore;
+}
+
+export const getMockStore = (preloadedState?: PreloadedState<RootState>) => {
+  return configureStore({
+    reducer: { todo: todoReducer },
+    preloadedState,
+  });
+};
+
+export function renderWithProviders(
+  ui: React.ReactElement,
+  {
+    preloadedState,
+    store = getMockStore(preloadedState),
+    ...renderOptions
+  }: ExtendedRenderOptions = {}
+) {
+  function Wrapper({ children }: PropsWithChildren): JSX.Element {
+    return <Provider store={store}>{children}</Provider>;
+  }
+  return { store, ...render(ui, { wrapper: Wrapper, ...renderOptions }) };
+}
+
