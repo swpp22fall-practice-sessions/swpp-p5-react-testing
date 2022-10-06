b6818848ebef720bbd837f51728ac796bdbdb089
askrid <joonwoo3023@gmail.com>
2022-10-06 19:45:00 +0900

    follow parctice session

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,15 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { store } from "./store";
 
 import App from "./App";
 
-test("renders learn react link", () => {
-  render(<App />);
+test("renders App.tsx", () => {
+  render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+  // eslint-disable-next-line testing-library/no-debugging-utils
+  screen.debug();
 });
---
+++ src/test-utils/mocks.ts
@@ -0,0 +1,22 @@
+import { configureStore, PreloadedState } from "@reduxjs/toolkit";
+import { RootState } from "../store";
+import todoReducer from "../store/slices/todo";
+
+export const getMockStore = (preloadedState?: PreloadedState<RootState>) => {
+  return configureStore({
+    reducer: { todo: todoReducer },
+    preloadedState,
+  });
+};
+
+export const mockNavigate = jest.fn();
+jest.mock("react-router", () => ({
+  ...jest.requireActual("react-router"),
+  useNavigate: () => mockNavigate,
+}));
+
+export const mockDispatch = jest.fn();
+jest.mock("react-redux", () => ({
+  ...jest.requireActual("react-redux"),
+  useDispatch: () => mockDispatch,
+}));
